c363f8ea2e43ec89dfa7c74cb47b86b8266b596e
Brent Rowland <brent.rowland@ttiltd.com>
2023-12-26 09:17:10 -0700

    Create app skeleton.

---
+++ src/main.zig
@@ -0,0 +1,38 @@
+const std = @import("std");
+const clap = @import("clap");
+
+const debug = std.debug;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    _ = gpa.deinit();
+    var allocator = gpa.allocator();
+
+    const params = comptime clap.parseParamsComptime(
+        \\-h, --help         Display this help and exit.
+        \\-v, --version      Output version information and exit.
+        \\-i, --input <str>  Input file.
+        \\-o, --output <str> Output file.
+        \\<string>...
+        \\
+    );
+
+    var diag = clap.Diagnostic{};
+    var res = clap.parse(clap.Help, &params, clap.parsers.default, .{
+        .diagnostic = &diag,
+        .allocator = allocator,
+    }) catch |err| {
+        // Report useful error and exit
+        diag.report(std.io.getStdErr().writer(), err) catch {};
+        return err;
+    };
+    defer res.deinit();
+
+    // Use the parsed arguments
+    if (res.args.input) |input|
+        debug.print("Input file: '{s}'\n", .{input});
+    if (res.args.output) |output|
+        debug.print("Output file: '{s}'\n", .{output});
+    for (res.positionals) |pos|
+        debug.print("{s}\n", .{pos});
+}
