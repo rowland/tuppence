16f5976669cc87b8a4633f4ae1f6a1c03b442f4c
Brent Rowland <brent.rowland@ttiltd.com>
2026-02-14 17:13:36 -0800

    Refactor punctuation matching using higher-order functions.

--- tup/parse/punctuation.go
+++ tup/parse/punctuation.go
@@ -2,32 +2,28 @@ package parse
 
 import "github.com/rowland/tuppence/tup/tok"
 
+func expectFunc(tokenType tok.TokenType) func([]tok.Token) (remainder []tok.Token, err error) {
+	return func(tokens []tok.Token) (remainder []tok.Token, err error) {
+		remainder = skipComments(tokens)
+		if peek(remainder).Type != tokenType {
+			return remainder, errorExpecting(tok.TokenTypes[tokenType], remainder)
+		}
+		return remainder[1:], nil
+	}
+}
+
 // at = "@" .
 
-func At(tokens []tok.Token) (remainder []tok.Token, err error) {
-	remainder = skipComments(tokens)
-	if peek(remainder).Type != tok.TokAt {
-		return nil, errorExpecting(tok.TokenTypes[tok.TokAt], remainder)
-	}
-	return remainder[1:], nil
-}
+var At = expectFunc(tok.TokAt)
 
 // colon = ":" .
 
-func Colon(tokens []tok.Token) (remainder []tok.Token, err error) {
-	remainder = skipComments(tokens)
-	if peek(remainder).Type != tok.TokColon {
-		return nil, errorExpecting(tok.TokenTypes[tok.TokColon], remainder)
-	}
-	return remainder[1:], nil
-}
+var Colon = expectFunc(tok.TokColon)
 
 // dot = "." .
 
-func Dot(tokens []tok.Token) (remainder []tok.Token, err error) {
-	remainder = skipComments(tokens)
-	if peek(remainder).Type != tok.TokDot {
-		return nil, errorExpecting(tok.TokenTypes[tok.TokDot], remainder)
-	}
-	return remainder[1:], nil
-}
+var Dot = expectFunc(tok.TokDot)
+
+// eol = "\r\n" | "\r" | "\n" .
+
+var EOL = expectFunc(tok.TokEOL)
