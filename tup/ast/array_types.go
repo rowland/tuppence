6771caa36bacccd789249868c6575346e50c52e2
Brent Rowland <brent.rowland@ttiltd.com>
2026-03-20 22:04:53 -0700

    Merge AST array helpers into array_types.go

--- tup/ast/array_types.go
+++ tup/ast/array_types.go
@@ -1,5 +1,15 @@
 package ast
 
+// size = decimal_literal | identifier .
+
+type Size interface {
+	Node
+	sizeNode()
+}
+
+func (n *IntegerLiteral) sizeNode() {}
+func (n *Identifier) sizeNode()     {}
+
 type ArrayElementType interface {
 	Node
 	arrayElementTypeNode()
